fdf26d72e3710d652513d4e69700c4e1e093632d
haripriya007 <harimsv007@gmail.com>
2025-08-07 13:19:17 +0530

    test: support custom delimiter from input string

--- testStringCal.py
+++ testStringCal.py
@@ -13,6 +13,8 @@ class TestStringCal(unittest.TestCase):
         self.assertEqual(add("1,2,3,4"), 10)
     def test_newlineAsDelimiter(self):
         self.assertEqual(add("1\n2,3"), 6)
+    def test_customDelimiter(self):
+        self.assertEqual(add("//;\n1;2"), 3)
      
 if __name__ == '__main__':
     unittest.main()
