673a5efff988aa3fa80ecb70816b63d2024eea9b
haripriya007 <harimsv007@gmail.com>
2025-08-07 12:46:51 +0530

    test: return number itself when single number is given

--- testStringCal.py
+++ testStringCal.py
@@ -5,6 +5,8 @@ class TestStringCal(unittest.TestCase):
 
     def test_emptyStringReturnsZero(self):
         self.assertEqual(add(""), 0)
+    def test_singleNumberReturnsSameNumber(self):
+        self.assertEqual(add("4"), 4)
 
 if __name__ == '__main__':
     unittest.main()
