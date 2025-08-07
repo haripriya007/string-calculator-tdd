fbf0ea81db9b21e689ea26cb010950c99e30ba37
haripriya007 <harimsv007@gmail.com>
2025-08-07 14:21:23 +0530

    test: support multi-length custom delimiters

--- stringCal.py
+++ stringCal.py
@@ -1,17 +1,24 @@
+import re
+
 def add(numbers):
-    if numbers == "":
+    if numbers.strip() == "": #handle whitespace
         return 0
-    delimiter = ","
+        
+    delimiters = [",", "\n"]   
     if numbers.startswith("//"):
-        parts = numbers.split("\n", 1)
-        delimiter = parts[0][2:]
-        numbers = parts[1]
-    numbers = numbers.replace("\n", delimiter)
-    parts = numbers.split(delimiter)
+        delimiter_part, numbers = numbers.split("\n", 1)
+        custom_delimiters =  re.findall(r"\[(.*?)\]", delimiter_part)#multi length delimiters in formatt
+        if custom_delimiters:
+            delimiters = custom_delimiters
+        else:
+            delimiters = [delimiter_part[2:]]
+
+    pattern = "|".join(map(re.escape, delimiters))
+    num_list = [int(n) for n in re.split(pattern, numbers) if n.strip()]
+
     
-    nums = list(map(int, parts))
-    negatives = [str(n) for n in nums if n < 0]
+    negatives = [n for n in num_list if n < 0]
     if negatives:
-        raise Exception("negative numbers not allowed " + ",".join(negatives))
-    return sum(nums)
+        raise Exception("negative numbers not allowed " + ", ".join(map(str, negatives)))
+    return sum(n for n in num_list if n <= 1000)#ignore number greated than 1000
     
