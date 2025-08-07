a53480d1a5a1059f46bdc4ee5b6ebe08ca0151d8
haripriya007 <harimsv007@gmail.com>
2025-08-07 13:04:28 +0530

    test: support adding multiple comma-separated numbers

--- testStringCal.py
+++ testStringCal.py
@@ -9,6 +9,8 @@ class TestStringCal(unittest.TestCase):
         self.assertEqual(add("4"), 4)
     def test_twoNumbersReturnsSum(self):
         self.assertEqual(add("1,2"), 3)
+    def test_multipleNumbersReturnsSum(self):
+        self.assertEqual(add("1,2,3,4"), 10)
      
 if __name__ == '__main__':
     unittest.main()
