065985b25d4902abc40cb34665d7b24424467de7
haripriya007 <harimsv007@gmail.com>
2025-08-07 13:58:52 +0530

    test: throw exception when negative numbers are passsed

--- stringCal.py
+++ stringCal.py
@@ -8,5 +8,10 @@ def add(numbers):
         numbers = parts[1]
     numbers = numbers.replace("\n", delimiter)
     parts = numbers.split(delimiter)
-    return sum(map(int, parts))
+    
+    nums = list(map(int, parts))
+    negatives = [str(n) for n in nums if n < 0]
+    if negatives:
+        raise Exception("negative numbers not allowed " +, "."join(negatives))
+    return sum(nums)
     
--- testStringCal.py
+++ testStringCal.py
@@ -15,6 +15,11 @@ class TestStringCal(unittest.TestCase):
         self.assertEqual(add("1\n2,3"), 6) #newline as delimiter
     def test_customDelimiter(self):
         self.assertEqual(add("//;\n1;2"), 3) #custom delimiter
+    def test_negativeNumbersThrowException(self):
+        with self.assertRaises(Exception) as context:
+            add("1,-2,3,-4")
+        self.assertIn("negative numbers not allowed -2,-4", str(context.exception))#neg number will throw exception
+
 
 
      
