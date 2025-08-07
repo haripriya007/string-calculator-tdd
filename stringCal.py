5b60fd327de200ec952c0a6ef28e6b18f87e4f6d
haripriya007 <harimsv007@gmail.com>
2025-08-07 14:45:21 +0530

    test: Added print statements and results

--- stringCal.py
+++ stringCal.py
@@ -19,18 +19,22 @@ def extractDelimiters(input_string):
     
 def add(numbers):
     if numbers.strip() == "": #handle whitespace
+        print("Input is empty or whitespace, Returning 0")
         return 0
     delimiters = extractDelimiters(numbers)
-    
+    print(f"Delimiters used: {delimiters}")
     if numbers.startswith("//"):
         numbers = numbers.split("\n", 1)[1]
         
     pattern = "|".join(map(re.escape, delimiters))
     num_list = [int(n) for n in re.split(pattern, numbers) if n.strip()]
-
+    print(f"Parsed numbers: {num_list}")
     
     negatives = [n for n in num_list if n < 0]
     if negatives:
         raise Exception("negative numbers not allowed " + ", ".join(map(str, negatives)))
-    return sum(n for n in num_list if n <= 1000)#ignore number greated than 1000
+    result = sum(n for n in num_list if n <= 1000)
+    #ignore number greated than 1000
+    print(f"Final sum (ignoring >1000): {result}")
+    return result
     
